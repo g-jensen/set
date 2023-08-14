d82e7835919474aeb4a7119f6ff671a4ae710ccb
g-jensen <greg.micah.jensen@gmail.com>
2023-08-14 00:10:25 -0400

    Created UI

--- spec/cljs/set/components_spec.cljs
+++ spec/cljs/set/components_spec.cljs
@@ -1,8 +1,19 @@
 (ns set.components-spec
   (:require-macros [speclj.core :refer [describe context it should= should-contain should should-not before]])
   (:require [speclj.core]
+            [set.components :as components]
             [set.main :as main]))
 
 (describe "A Collection of TicTacToe Components"
-  (it "fails"
-    (should= main/attributes 1)))
+
+  (it "converts a card to a file path"
+    (should= "cards/green-one-diamond-open.png"
+             (components/card->path (main/card :green :one :diamond :open)))
+    (should= "cards/red-one-diamond-open.png"
+             (components/card->path (main/card :red :one :diamond :open)))
+    (should= "cards/red-two-diamond-open.png"
+             (components/card->path (main/card :red :two :diamond :open)))
+    (should= "cards/red-two-squiggle-open.png"
+             (components/card->path (main/card :red :two :squiggle :open)))
+    (should= "cards/red-two-squiggle-solid.png"
+             (components/card->path (main/card :red :two :squiggle :solid)))))
--- src/cljs/set/components.cljs
+++ src/cljs/set/components.cljs
@@ -1,5 +1,29 @@
 (ns set.components
-  (:require [set.main :as main]))
+  (:require [reagent.core :as r]
+            [set.main :as main]
+            [set.state :as state]))
+
+(def state (r/atom (state/initial-state main/deck shuffle)))
+
+(defn card->path [card]
+  (str "cards/"
+       (name (:color card)) "-"
+       (name (:count card)) "-"
+       (name (:shape card)) "-"
+       (name (:shade card)) ".png"))
+
+(defn card->button [idx card]
+  [:input {:type "image"
+           :src (card->path card)
+           :on-click #(swap! state state/next-state (inc idx))
+           :border (if (some #{card} (:selected-cards @state))
+                     "2px")}])
+
+(defn vec-insert [vec element idx]
+  (concat (subvec vec 0 idx) [element] (subvec vec idx)))
+
 (defn main []
   [:div
-   [:h1 main/deck]])
+   (-> (map-indexed card->button (:cards @state)) (vec)
+       (vec-insert [:br] 4) (vec)
+       (vec-insert [:br] 9))])
