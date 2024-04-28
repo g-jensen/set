640108a0542add480f471595939db1e577f9399b
g-jensen <greg.micah.jensen@gmail.com>
2024-04-28 18:57:02 -0400

    add game

--- src/cljs/set/state.cljs
+++ src/cljs/set/state.cljs
@@ -2,6 +2,7 @@
   (:require [c3kit.bucket.api :as db]
             [reagent.core :as reagent]))
 
+(def game (reagent/track #(db/ffind :game)))
 (def nickname (reagent/atom nil))
 (def room (reagent/track #(db/ffind :room)))
 (def players (reagent/track #(map db/entity (:players @room))))
