5c95fad2dcf397a8de717ab31cb666fe02ee094f
g-jensen <greg.micah.jensen@gmail.com>
2024-04-29 01:23:58 -0400

    delete stats

--- src/cljs/set/stats.cljs
+++
@@ -1,7 +0,0 @@
-(ns set.stats
-  (:require [set.cardsc :as cardsc]))
-
-(defn stats [state-ratom]
-  [:div
-   [:h1.no-margin {:id "-found"} (str "Found: " (:found-sets-count @state-ratom))]
-   [:p.no-margin {:id "-exist"} (str "Exist: " (cardsc/set-count (:cards @state-ratom)))]])
