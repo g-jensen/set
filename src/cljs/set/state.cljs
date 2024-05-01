1ea039e1d328bc2c59b6a444e6afddb2291353ca
g-jensen <greg.micah.jensen@gmail.com>
2024-05-01 16:03:33 -0400

    room is found via page

--- src/cljs/set/state.cljs
+++ src/cljs/set/state.cljs
@@ -1,8 +1,9 @@
 (ns set.state
   (:require [c3kit.bucket.api :as db]
-            [reagent.core :as reagent]))
+            [reagent.core :as reagent]
+            [set.page :as page]))
 
 (def game (reagent/track #(db/ffind :game)))
 (def nickname (reagent/atom nil))
-(def room (reagent/track #(db/ffind :room)))
+(def room (reagent/track #(db/ffind-by :room :code (:room-code @page/state))))
 (def players (reagent/track #(map db/entity (:players @room))))
