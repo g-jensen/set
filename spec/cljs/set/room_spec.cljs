6f4d840fce827d85a71080f5fbe224afd6521745
g-jensen <greg.micah.jensen@gmail.com>
2024-05-01 19:22:32 -0400

    add joining and leaving room

--- spec/cljs/set/room_spec.cljs
+++ spec/cljs/set/room_spec.cljs
@@ -4,6 +4,7 @@
   (:require [c3kit.apron.corec :as ccc]
             [c3kit.bucket.api :as db]
             [c3kit.wire.spec-helper :as wire]
+            [reagent.core :as reagent]
             [set.cardsc :as cardsc]
             [set.cardsc-spec :as cardsc-spec]
             [set.fallout :as fo]
@@ -19,17 +20,94 @@
   (with-stubs)
   (wire/stub-ws)
   (wire/with-root-dom)
+  (fo/with-schemas)
   (before (db/clear)
+          (fo/init)
           (sut/install-room! fo/mojave-code)
           (routes/load-page! :room))
-  (fo/init-with-schemas)
 
   (context "entering"
     (it "fetches room"
       (should-have-invoked :ws/call! {:with [:room/fetch {:room-code fo/mojave-code} db/tx*]})))
 
+  (context "nickname prompt"
+    (before (wire/render [sut/nickname-prompt state/nickname]))
+
+    (it "updates input on change"
+      (wire/change! "#-nickname-input" "Lautrec")
+      (should= "Lautrec" (wire/value "#-nickname-input"))
+      (wire/change! "#-nickname-input" "Patches")
+      (should= "Patches" (wire/value "#-nickname-input")))
+
+    (context "button click"
+      (before (reset! state/nickname nil))
+
+      (context "updates nickname ratom"
+        (it "when nickname is Lautrec"
+          (wire/change! "#-nickname-input" "Lautrec")
+          (should= nil @state/nickname)
+          (wire/click! "#-join-button")
+          (should= "Lautrec" @state/nickname))
+
+        (it "when nickname is Patches"
+          (wire/change! "#-nickname-input" "Patches")
+          (should= nil @state/nickname)
+          (wire/click! "#-join-button")
+          (should= "Patches" @state/nickname)))
+
+      (it "joins room"
+        (wire/change! "#-nickname-input" "Lautrec")
+        (wire/click! "#-join-button")
+        (should-have-invoked :ws/call! {:with [:room/join
+                                               {:nickname "Lautrec" :room-code "mojave"}
+                                               db/tx*]}))
+
+      (it "doesn't join room if blank nickname"
+        (wire/change! "#-nickname-input" " ")
+        (should= nil @state/nickname)
+        (wire/click! "#-join-button")
+        (should-not-have-invoked :ws/call!))))
+
+  (context "nickname prompt or room"
+    (before (wire/render [sut/nickname-prompt-or-room state/nickname]))
+
+    (it "renders nickname prompt if blank nickname"
+      (reset! state/nickname "   ")
+      (wire/flush)
+      (should-select "#-nickname-prompt")
+      (should-not-select "#-room"))
+
+    (it "renders room if nickname"
+      (reset! state/nickname "Yes Man")
+      (wire/flush)
+      (should-not-select "#-nickname-prompt")
+      (should-select "#-room")))
+
+  (context "maybe render room"
+    (before (wire/render [sut/maybe-render-room sut/room]))
+
+    (it "renders error if no room"
+      (sut/install-room! "m0jave")
+      (wire/flush)
+      (should-select "#-room-not-found")
+      (should-not-select "#-nickname-prompt-or-room"))
+
+    ; can't figure out why I can't db/tx mojave to have started state
+    (it "renders error if trying to join when room already started"
+      (with-redefs [sut/get-me (constantly nil)
+                    sut/room   (reagent/atom (assoc @sut/room :state :started))]
+        (wire/render [sut/maybe-render-room sut/room])
+        (should-select "#-room-started")))
+
+    (it "renders room"
+      (with-redefs [sut/get-me (constantly @fo/benny)]
+        (wire/render [sut/maybe-render-room sut/room])
+        (should-not-select "#-room-started")
+        (should-not-select "#-room-not-found")
+        (should-select "#-prompt-or-room"))))
+
   (context "players display"
-    (before (wire/render [set.layout/default]))
+    (before (wire/render [sut/full-players sut/room sut/players]))
 
     (it "exists"
       (should-select "#-players"))
--- src/cljs/set/room.cljs
+++ src/cljs/set/room.cljs
@@ -1,18 +1,21 @@
 (ns set.room
   (:require [c3kit.apron.corec :as ccc]
+            [c3kit.wire.js :as wjs]
             [c3kit.wire.websocket :as ws]
             [clojure.string :as str]
             [reagent.core :as reagent]
             [set.cardsc :as cardsc]
             [set.page :as page]
             [c3kit.bucket.api :as db]
-            [c3kit.wire.util :as util]))
+            [c3kit.wire.util :as util]
+            [set.state :as state]
+            [set.playerc :as playerc]))
 
 (def state (page/cursor [:room] {}))
 (defn install-room! [code]
   (swap! state assoc :code code))
 (def code (reagent/track #(:code @state)))
-(def room (reagent/track #(db/ffind :room :code @code)))
+(def room (reagent/track #(db/ffind-by :room :code @code)))
 (def players (reagent/track #(map db/entity (:players @room))))
 
 (defn- how-to-play []
@@ -44,35 +47,76 @@
   (= (:id player) (:host room)))
 
 (defn- join-room! []
-  (prn "join")
-  (ws/call! :room/join
-            {:nickname "new user" :room-code (:room-code @page/state)}
-            db/tx*))
+  (when-not (str/blank? @state/nickname)
+    (ws/call! :room/join
+              {:nickname @state/nickname :room-code @code}
+              db/tx*)))
+
+(defn full-players [room-ratom players-ratom]
+  [:div
+   {:id "-players"}
+   [:h3 "Players"]
+   [:ul
+    [:<>
+     (util/with-react-keys
+       (ccc/for-all [player @players-ratom]
+                    [:li {:id (str "-player-" (:id player))}
+                     (str (:nickname player) (when (host? @room-ratom player) " (Host)"))]))]]])
 
 (defn full-room [room-ratom players-ratom]
   [:div.main-container
+   {:id "-room"}
    [:div.left-container
     [:br]
     [:br]
-    [:div
-     {:id "-players"}
-     [:h3 "Players"]
-     [:ul
-      [:<>
-       (util/with-react-keys
-         (ccc/for-all [player @players-ratom]
-                      [:li {:id (str "-player-" (:id player))}
-                       (str (:nickname player) (when (host? @room-ratom player) " (Host)"))]))]]]]
+    [full-players room-ratom players-ratom]]
    [:div.center
     [:div.game-container
-     [:p (str @room-ratom)]
      [:h1.text-align-center "Set"]
      [:<>
       [:h2.center.categories-data "Waiting for host to start game..."]
       (how-to-play)
       [:div.center
-       [:button {:on-click join-room!}
-        "Start Game"]]]]]])
+       [:button "Start Game"]]]]]])
+
+(defn nickname-prompt [_]
+  (let [local-nickname-ratom (reagent/atom nil)]
+    (fn [nickname-ratom]
+      [:div.center-div.margin-top-plus-5
+       {:id "-nickname-prompt"}
+       [:h1 "Enter nickname to join room..."]
+       [:div.center
+        [:input {:type "text"
+                 :id "-nickname-input"
+                 :placeholder "Enter your nickname"
+                 :value @local-nickname-ratom
+                 :on-change #(reset! local-nickname-ratom (wjs/e-text %))}]
+        [:button {:id       "-join-button"
+                  :on-click #(do (reset! nickname-ratom @local-nickname-ratom)
+                                 (join-room!))}
+         "Join"]]])))
+
+(defn nickname-prompt-or-room [nickname-ratom]
+  [:div {:id "-prompt-or-room"}
+   (if (str/blank? @nickname-ratom)
+     [nickname-prompt nickname-ratom]
+     [full-room room players])])
+
+(defn get-me []
+  (when ws/client
+    (playerc/by-conn-id (:id (:connection @ws/client)))))
+
+(defn- lobby? [room]
+  (= :lobby (:state room)))
+
+(defn maybe-render-room [room-ratom]
+  (cond
+    (not @room-ratom)
+      [:h1 {:id "-room-not-found"} "Room not found!"]
+    (or (lobby? @room-ratom) (get-me))
+      [nickname-prompt-or-room state/nickname]
+    :else
+      [:h1 {:id "-room-started"} "Room as already started. Try joining back later."]))
 
 (defn fetch-room! []
   (ws/call! :room/fetch
@@ -83,4 +127,4 @@
   (fetch-room!))
 
 (defmethod page/render :room [_]
-  [full-room room players])
+  [maybe-render-room room])
