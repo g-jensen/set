cb6414577622bf63100c41e8d71a70143cb3970d
g-jensen <greg.micah.jensen@gmail.com>
2024-05-02 03:20:25 -0400

    add ws-sumbit-cards

--- spec/clj/set/game_spec.clj
+++ spec/clj/set/game_spec.clj
@@ -3,7 +3,7 @@
             [c3kit.wire.apic :as apic]
             [set.cardsc :as cardsc]
             [set.cardsc-spec :as cardsc-spec]
-            [set.fallout :as fo :refer [mojave nipton yes-man boone benny]]
+            [set.fallout :as fo :refer [mojave nipton yes-man boone benny caravan]]
             [set.dispatch :as dispatch]
             [set.gamec :as gamec]
             [set.playerc :as playerc]
@@ -13,11 +13,10 @@
 
 (describe "game"
   (with-stubs)
-  (cardsc-spec/with-bad-shuffle)
   (fo/init-with-schemas)
+  (redefs-around [dispatch/push-to-players! (stub :push-to-players!)])
 
   (context "ws-start-game"
-    (redefs-around [dispatch/push-to-players! (stub :push-to-players!)])
 
     (it "fails if connection-id is not host"
       (let [non-host-player @boone
@@ -39,11 +38,39 @@
 
     (it "notifies players of game start"
       (let [response (sut/ws-start-game {:connection-id (:conn-id @yes-man)})
-            players (map db/entity (:players @mojave))]
+            players (map db/entity (:players @mojave))
+            new-game (last (db/find :game))
+            new-room (assoc @mojave :game (:id new-game))]
         (should= :ok (:status response))
         (should-have-invoked :push-to-players! {:with [players
                                                        :room/update
-                                                       [@mojave]]})
+                                                       [new-room]]})
         (should-have-invoked :push-to-players! {:with [players
                                                        :game/update
-                                                       (gamec/->game (shuffle cardsc/deck))]})))))
+                                                       new-game]}))))
+
+  (context "ws-submit-cards"
+    (it "updates game"
+      (let [selected-cards (take 3 cardsc/deck)
+            old-game @caravan
+            response (sut/ws-submit-cards {:connection-id (:conn-id @yes-man)
+                                           :params {:selected-cards selected-cards}})]
+        (should= :ok (:status response))
+        (should= (gamec/process-card-submission! old-game selected-cards) @caravan)))
+
+    (it "notifies players if selected cards is a set"
+      (let [players (map db/entity (:players @mojave))
+            selected-cards (take 3 cardsc/deck)
+            new-game (gamec/process-card-submission @caravan selected-cards)
+            response (sut/ws-submit-cards {:connection-id (:conn-id @yes-man)
+                                           :params {:selected-cards selected-cards}})]
+        (should= :ok (:status response))
+        (should-have-invoked :push-to-players! {:with [players
+                                                       :game/update
+                                                       new-game]})))
+
+    (it "doesn't notify players if selected cards is not a set"
+      (let [response (sut/ws-submit-cards {:connection-id (:conn-id @yes-man)
+                                           :params {:selected-cards []}})]
+        (should= :ok (:status response))
+        (should-not-have-invoked :push-to-players!)))))
--- src/clj/set/game.clj
+++ src/clj/set/game.clj
@@ -17,10 +17,18 @@
         room (db/ffind-by :room :host (:id player))]
     (if-not room
       (apic/fail nil "Only the host can start the game!")
-      (let [room (roomc/start! room)]
+      (let [game (gamec/create-game! (shuffle cardsc/deck))
+            room (roomc/assign-game-to-room! game (roomc/start room))]
         (room/push-room! room)
-        (push-game-to-room! (gamec/->game (shuffle cardsc/deck)) room)
+        (push-game-to-room! game room)
         (apic/ok room)))))
 
 (defn ws-submit-cards [{:keys [params connection-id] :as request}]
-  )
+  (let [player (playerc/by-conn-id connection-id)
+        room (db/ffind-by :room :host (:id player))
+        game (db/ffind-by :game :id (:game room))
+        selected-cards (:selected-cards params)
+        updated-game (gamec/process-card-submission! game selected-cards)]
+    (when (cardsc/set? selected-cards)
+      (push-game-to-room! updated-game room))
+    (apic/ok)))
