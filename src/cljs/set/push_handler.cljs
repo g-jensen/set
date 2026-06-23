90255f2d6cd56b7d26f0fb253fe941bcc584128d
Alex Jensen <alex@endure.io>
2026-06-23 13:25:25 -0400

    bumps wire

--- src/cljs/set/push_handler.cljs
+++ src/cljs/set/push_handler.cljs
@@ -1,14 +1,14 @@
 (ns set.push-handler
   (:require [c3kit.bucket.api :as db]
-            [c3kit.wire.websocket :as ws]
+            [c3kit.wire.core.websocket :as ws]
             [set.game :as game]
             [set.state :as state]))
 
-(defmethod ws/push-handler :room/update [push]
+(defmethod ws/push-handler :room/update [_state push]
   (db/tx* (:params push))
   (swap! state/push-count inc))
 
-(defmethod ws/push-handler :game/update [push]
+(defmethod ws/push-handler :game/update [_state push]
   (swap! game/state assoc :selected-cards [])
   (db/tx (:params push))
   (swap! state/push-count inc))
